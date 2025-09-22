b8f7cddcac6d8b9f3467393650ff13d9ef6e4fe1
Paridhi Agrawal <agrawalparidhi60@gmail.com>
2025-09-22 23:57:57 +0530

    lec_1.cpp

---
+++ lec_1.cpp
@@ -0,0 +1,16 @@
+/*
+In lecture-1 I learned about:
+  What id Data Structure ?
+  How is it classified ?
+  How can I solve the problems ?
+  What is Flowchart and PseudoCode ?
+
+And also What is the boilerplate code for cpp ?
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+  //code
+  return 0;  
+}

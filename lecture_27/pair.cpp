646c48f6970e965e9897c0c10e5f68ace93c3ace
Paridhi Agrawal <agrawalparidhi60@gmail.com>
2025-10-14 23:45:50 +0530

    lecture 27

--- lecture_27/pair.cpp
+++ lecture_27/pair.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
@@ -12,6 +13,15 @@ int main(){
     cout << p1.first << endl; // 1
     cout << p1.second.first << endl;  // 2
     cout << p1.second.second << endl;  // a
+
+    vector<pair<int, int>> vec = {{1,2}, {2,3}, {3,4}};
+    vec.emplace_back(4,5); // create in-place objects 
+    vec.push_back({5,6}); // inserts 
+
+    for(pair<int, int> val : vec){
+    // for(auto val : vec){
+        cout << val.first << " " << val.second << endl;;
+    }
 
     return 0;
 }
